1d2800405e27573f214b650c88cad9c9d700cad9
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-10-08 16:21:06 +0330

    feat: create project

---
+++ AgriculturalLandManagement/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using AgriculturalLandManagement.Data;
+using AgriculturalLandManagement.Repositories;
+using System.Reflection;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer(); // Required for Swagger
+
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+// Register DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register Repository
+builder.Services.AddScoped<ILandRepository, LandRepository>();
+
+var app = builder.Build();
+
+// Enable Swagger UI in development
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
