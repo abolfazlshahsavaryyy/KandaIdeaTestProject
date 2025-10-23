4fa5292962fdf0d611411deace23073520b1b61a
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-10-23 16:51:49 +0330

    feat: add the Dto for lands

---
+++ AgriculturalLandManagement/DTO/Land/LandReadDto.cs
@@ -0,0 +1,7 @@
+public class LandReadDto
+{
+    public int Id { get; set; }
+    public string OwnerName { get; set; } = string.Empty;
+    public string Production { get; set; } = string.Empty;
+    public double Area { get; set; }
+}
---
+++ AgriculturalLandManagement/DTO/Land/LandUpdateDto.cs
@@ -0,0 +1,6 @@
+public class LandUpdateDto
+{
+    public int Id { get; set; }
+    public string OwnerName { get; set; } = string.Empty;
+    public string Production { get; set; } = string.Empty;
+}
