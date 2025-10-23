3d8713ab5c27f93d262c7d479f3ab8733881a605
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-10-23 16:48:17 +0330

    feat: add corner Image repo

---
+++ AgriculturalLandManagement/Repositories/Interface/ICornerImageRepository.cs
@@ -0,0 +1,10 @@
+using AgriculturalLandManagement.Models;
+
+public interface ICornerImageRepository
+{
+    Task<IEnumerable<CornerImage>> GetAllAsync();
+    Task<CornerImage?> GetByIdAsync(int id);
+    Task<bool> CreateAsync(CornerImage image);
+    Task<bool> UpdateAsync(int id, byte[] imageData);
+    Task<bool> DeleteAsync(int id);
+}
